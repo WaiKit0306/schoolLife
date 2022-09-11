54eca71a120e9786ea509cef786f5301b27eb838
waikit <sekwaikit0306@gmail.com>
2022-09-11 22:48:12 +0800

    11/09/2022

--- app/src/main/java/com/example/schoollife/MainActivity.kt
+++ app/src/main/java/com/example/schoollife/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.schoollife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.schoollife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        replaceFragement(homeFragment())
+        binding.btmNavView.setOnItemSelectedListener {
+        when(it.itemId)
+        {
+            R.id.nav_home->replaceFragement(homeFragment())
+            R.id.nav_profile->replaceFragement(profileFragment())
+            else->{
+
+            }
+
+
+
+        }
+            true
+        }
     }
+
+    private fun replaceFragement(fragment : Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
 }
